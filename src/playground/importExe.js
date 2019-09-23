08989449322f955480a52bcbcc893812018879b8
Florence Nway <florence@Florences-iMac.local>
2019-09-23 15:07:47 +0100

    import export exercise

---
+++ src/playground/importExe.js
@@ -0,0 +1,11 @@
+//import './utils.js';
+import subtract,{square,add} from './utils.js';
+import {isAdult,canDrink} from './person.js';
+
+console.log('app.js is running');
+console.log(square(4));
+console.log(add(4,5));
+console.log(subtract(100,5));
+
+console.log(isAdult(17));
+canDrink(22);
